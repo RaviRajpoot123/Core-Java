3ba024221bbf3a47b97d0b6943cb5c1b7a519ee1
Ravi Rajpoot <103096346+ravirajpoot2204@users.noreply.github.com>
2024-03-26 15:24:44 +0530

    Add files via upload

---
+++ AO3.java
@@ -0,0 +1,6 @@
+class AO3{
+public static void main(String [] args){
+double a = 200, b = 10;
+System.out.println("This is an output : " + (a/b));
+} 
+}
---
+++ AO4.java
@@ -0,0 +1,6 @@
+class AO4{
+public static void main(String [] args){
+int a = 200, b = 10;
+System.out.println("This is an output : " + (a%b));
+} 
+}
