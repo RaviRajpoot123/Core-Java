163786b6a7ccc3ce443f196cfd91634106f20f96
Ravi Rajpoot <103096346+ravirajpoot2204@users.noreply.github.com>
2024-03-26 15:26:17 +0530

    Add files via upload

---
+++ ArithematicOperationThirdvar/AOT0.java
@@ -0,0 +1,7 @@
+class AOT0{
+public static void main(String [] args){
+int a = 200, b = 10;
+int c = a+b;
+System.out.println("This is an output : " +c);
+} 
+}
---
+++ ArithematicOperationThirdvar/AOT3.java
@@ -0,0 +1,7 @@
+class AOT3{
+public static void main(String [] args){
+double a = 200, b = 10;
+double c = a/b;
+System.out.println("This is an output : " +c);
+} 
+}
